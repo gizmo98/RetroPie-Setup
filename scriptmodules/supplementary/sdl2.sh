824e8fd3fe5183d0d157bcaab7dca5232e170a19
gizmo98 <atz.SE@gmx.de>
2014-10-27 19:22:16 +0100

    Add SDL2 mixer, image and ttf

--- scriptmodules/supplementary/sdl2.sh
+++ scriptmodules/supplementary/sdl2.sh
@@ -12,9 +12,13 @@ function sources_sdl() {
     # The rest should already be installed, but just to be safe, include them all.
 
     wget http://downloads.petrockblock.com/retropiearchives/SDL2-2.0.1.tar.gz
+    wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.12.zip
+    wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.0.zip
+    wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.zip
     mkdir -p "$rootdir/supplementary/"
     tar xvfz SDL2-2.0.1.tar.gz -C "$rootdir/supplementary/"
     rm SDL2-2.0.1.tar.gz || return 1
+    unzip \*.zip
 }
 
 function build_sdl() {
@@ -26,6 +30,18 @@ function build_sdl() {
     mv /usr/include/GL.bak /usr/include/GL
     mv /usr/include/GLES.bak /usr/include/GLES
     popd || return 1
+    pushd "$rootdir/supplementary/SDL2_ttf-2.0.12" || return 1
+    ./configure
+    make -j2
+    popd || return 1
+    pushd "$rootdir/supplementary/SDL2_image-2.0.0" || return 1
+    ./configure
+    make -j2
+    popd || return 1
+    pushd "$rootdir/supplementary/SDL2_mixer-2.0.0" || return 1
+    ./configure
+    make -j2
+    popd || return 1
 }
 
 function install_sdl() {
@@ -33,5 +49,14 @@ function install_sdl() {
     make install || return 1
     cp -R include /usr/include/
     mv /usr/include/include /usr/include/SDL2	
+    popd || return 1
+    pushd "$rootdir/supplementary/SDL2_ttf-2.0.12" || return 1
+    make install || return 1
+    popd || return 1
+    pushd "$rootdir/supplementary/SDL2_image-2.0.0" || return 1
+    make install || return 1
+    popd || return 1
+    pushd "$rootdir/supplementary/SDL2_mixer-2.0.0" || return 1
+    make install || return 1
     popd || return 1
 }
