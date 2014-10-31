7db70aef4ffbaf696dde2b4ac045854b399b2295
gizmo98 <atz.SE@gmx.de>
2014-10-31 16:57:45 +0100

    Update lima.sh

--- scriptmodules/supplementary/lima.sh
+++ scriptmodules/supplementary/lima.sh
@@ -3,7 +3,7 @@ rp_module_desc="Build lima driver"
 rp_module_menus="4+"
 
 function install_lima() {
-  gitPullOrClone "git://gitorious.org/lima/lima.git"
+  gitPullOrClone "$rootdir/supplementary/lima" "git://gitorious.org/lima/lima.git"
   cd lima
   make
   make install
