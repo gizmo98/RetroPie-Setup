b190e45a50040ad658c9f246c9d5866ce451ff61
gizmo98 <atz.SE@gmx.de>
2014-11-02 12:25:58 +0100

    Update mupen64.sh

--- scriptmodules/libretrocores/mupen64.sh
+++ scriptmodules/libretrocores/mupen64.sh
@@ -3,17 +3,19 @@ rp_module_desc="N64 LibretroCore Mupen64Plus"
 rp_module_menus="4+"
 
 function sources_mupen64plus() {
-    gitPullOrClone "$rootdir/emulatorcores/mupen64plus" git://github.com/libretro/mupen64plus-libretro.git
+    gitPullOrClone "$rootdir/emulatorcores/mupen64plus" git://github.com/gizmo98/mupen64plus-libretro.git
     pushd "$rootdir/emulatorcores/mupen64plus"
     popd
 }
 
 function build_mupen64plus() {
     pushd "$rootdir/emulatorcores/mupen64plus"
+    # Add missing path
+    # sed -i 's|GL_LIB := -lGLESv2|GL_LIB := -L/opt/vc/lib -lGLESv2|g' Makefile
     make clean
-    make -j2 platform=armvhardfloatunixneongles WITH_DYNAREC=arm CPUFLAGS=" -mcpu=cortex-a7 -mfpu=neon-vfp4 -mfloat-abi=hard -DNO_ASM -DNOSSE" GLIDE2GL=0
+    make platform=a20 
     if [[ -z `find $rootdir/emulatorcores/mupen64plus/ -name "*libretro*.so"` ]]; then
-        __ERRMSGS="$__ERRMSGS Could not successfully compile GBA core."
+        __ERRMSGS="$__ERRMSGS Could not successfully compile N64 core."
     fi
     popd
 }
@@ -23,12 +25,10 @@ function configure_mupen64plus() {
 
     ensureSystemretroconfig "n64"
 
-    ensureKeyValue "mupen64-gfxplugin" "gln64" "$rootdir/configs/all/retroarch-core-options.cfg"
+    ensureKeyValue "mupen64-gfxplugin" "rice" "$rootdir/configs/all/retroarch-core-options.cfg"
     ensureKeyValue "mupen64-gfxplugin-accuracy" "low" "$rootdir/configs/all/retroarch-core-options.cfg"
     ensureKeyValue "mupen64-screensize" "320x240" "$rootdir/configs/all/retroarch-core-options.cfg"
 
     rps_retronet_prepareConfig
     setESSystem "Nintendo 64" "n64" "~/RetroPie/roms/n64" ".z64 .Z64 .n64 .N64 .v64 .V64" "$rootdir/supplementary/runcommand/runcommand.sh 1 \"$rootdir/emulators/RetroArch/installdir/bin/retroarch -L `find $rootdir/emulatorcores/mupen64plus-libretro/ -name \"*libretro*.so\" | head -1` --config $rootdir/configs/all/retroarch.cfg --appendconfig $rootdir/configs/n64/retroarch.cfg $__tmpnetplaymode$__tmpnetplayhostip_cfile $__tmpnetplayport$__tmpnetplayframes %ROM%\"" "n64" "n64"
-    # <!-- alternatively: <command>$rootdir/emulators/snes9x-rpi/snes9x %ROM%</command> -->
-    # <!-- alternatively: <command>$rootdir/emulators/pisnes/snes9x %ROM%</command> -->
 }
