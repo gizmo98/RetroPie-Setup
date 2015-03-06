34a8fff204ac27eddea97b187a0b06bc57853fff
gizmo98 <atz.SE@gmx.de>
2015-03-06 20:59:45 +0100

    Refine messages.

--- scriptmodules/supplementary/bluezps3.sh
+++ scriptmodules/supplementary/bluezps3.sh
@@ -4,12 +4,13 @@ rp_module_menus="4+"
 rp_module_flags="nobin"
 
 function configure_bluezps3() {  
-  printMsgs "dialog" "Please connect your PS3 controller via USB-CABLE and press ENTER."
+  printMsgs "dialog" "Please connect your PS3 controller via USB-CABLE, press PS button and ENTER."
   # wait 5 seconds so bluez has enough to create directories
   sleep 5
   for file in $(grep -l "Name=PLAYSTATION(R)3 Controller" /var/lib/bluetooth/*/*/info); do
     sed -i "s/Trusted=false/Trusted=true/g" $file
   done
   
+  printMsgs "dialog" "Please restart."
   /etc/init.d/bluetooth restart
 }
