a3bdf5852107f32ed7843ef0432a348d8afab996
gizmo98 <gizmo98@users.noreply.github.com>
2015-03-07 16:44:12 +0100

    Update bluezps3.sh

--- scriptmodules/supplementary/bluezps3.sh
+++ scriptmodules/supplementary/bluezps3.sh
@@ -3,10 +3,14 @@ rp_module_desc="Pair PS3 bluetooth controller (BLUEZ)"
 rp_module_menus="4+"
 rp_module_flags="nobin"
 
+function depends_bluezps3() {
+    getDepends inotify-tools
+}
+
 function configure_bluezps3() {  
-  printMsgs "dialog" "Please connect your PS3 controller via USB-CABLE, press PS button and ENTER."
-  # wait 5 seconds so bluez has enough to create directories
-  sleep 5
+  echo "dialog" "Please connect your PS3 controller via USB-CABLE, press PS button and ENTER."
+  # Wait max 30s until directories appear
+  inotifywait -e create -r -t 30 /var/lib/bluetooth
   for file in $(grep -l "Name=PLAYSTATION(R)3 Controller" /var/lib/bluetooth/*/*/info); do
     sed -i "s/Trusted=false/Trusted=true/g" $file
   done
